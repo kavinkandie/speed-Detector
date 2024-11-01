a7ea64e2a2ad9f3011a89e12396445d5330dcab9
kevinkandie <kevinkan500@gmail.com>
2024-11-01 13:15:53 +0300

    finds the Grading of students.

--- grades.js
+++ grades.js
@@ -1,25 +1,26 @@
-function myGrading(score) {
-    let result
-  
-    switch(true) {
-      case (score >=79):
-         result ='distinction';
+function myGrading(name, score) {
+  let result;
+
+  switch(true) {
+      case (score >= 79):
+          result = 'A';
           break;
-      case (result <= 78 && score >= 60):
-          result = 'credit';
-           break; 
-      case (score <= 59 && score>=50):
-          result = 'pass';
-           break;
-         case (score <= 49 && score >= 40):
-          result = 'fail';
-        break;
-      default: (score <= 39)
-          result = 'invalid score';
-         
-  
-   }
-   return result;
-   }
-   console.log(myGrading(55));
-  
+      case (score >= 60):
+          result = 'B';
+          break; 
+      case (score >= 50):
+          result = 'C';
+          break;
+      case (score >= 40):
+          result = 'D';
+          break;
+      default:
+          result = 'E';
+  }
+
+  return `${name}: ${result}`;
+}
+console.log(myGrading('sam', 55));   
+console.log(myGrading('june', 60));  
+console.log(myGrading('mark', 45));   
+console.log(myGrading('kiptoo', 71)); 
